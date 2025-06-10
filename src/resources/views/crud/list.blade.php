2b239012b33661f789f4502fbd1a54f0fa47363a
Pedro Martins <pxpm88@gmail.com>
2025-06-10 14:45:30 +0100

    move the list heading to the datatable component (#5808)

--- src/resources/views/crud/list.blade.php
+++ src/resources/views/crud/list.blade.php
@@ -11,13 +11,6 @@
   $breadcrumbs = $breadcrumbs ?? $defaultBreadcrumbs;
 @endphp
 
-@section('header')
-    <section class="header-operation container-fluid animated fadeIn d-flex mb-2 align-items-baseline d-print-none" bp-section="page-header">
-        <h1 class="text-capitalize mb-0" bp-section="page-heading">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</h1>
-        <p class="ms-2 ml-2 mb-0" id="datatable_info_stack" bp-section="page-subheading">{!! $crud->getSubheading() ?? '' !!}</p>
-    </section>
-@endsection
-
 @section('content')
   {{-- Default box --}}
   <div class="row" bp-section="crud-operation-list">
